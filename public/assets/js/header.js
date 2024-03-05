bf40a315b9d0d4cbe77e7d82de29276d8188a0d2
Kaique <kaiqueols75@gmail.com>
2024-03-05 00:54:31 -0300

    feat: creating navigation component

--- public/assets/js/header.js
+++ public/assets/js/header.js
@@ -1,3 +1,12 @@
-function openDropdown(){
-    document.querySelector('.dropdown-container').classList.toggle('active');    
+function openDropdown() {
+    document.querySelector('.dropdown-container').classList.toggle('active');
 }
+
+document.addEventListener('click', function(event) {
+    var dropdown = document.querySelector('.dropdown-container');
+    var button = document.querySelector('.user-container button');
+
+    if (!dropdown.contains(event.target) && !button.contains(event.target)) {
+        dropdown.classList.remove('active');
+    }
+});
